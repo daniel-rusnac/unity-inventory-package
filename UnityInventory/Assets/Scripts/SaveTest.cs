88cbc134063566f826078000c91e3fb0a99ca2e5
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2022-01-21 00:50:21 +0200

    Added ES3 for testing

---
+++ UnityInventory/Assets/Scripts/SaveTest.cs
@@ -0,0 +1,30 @@
+﻿using InventorySystem;
+using UnityEngine;
+
+public class SaveTest : MonoBehaviour
+{
+    public Inventory _inventory;
+
+    private void Awake()
+    {
+        _inventory = new Inventory();
+    }
+
+    [ContextMenu("Clear")]
+    private void ClearSave()
+    {
+        ES3.DeleteFile();
+    }
+
+    [ContextMenu("Save")]
+    private void Save()
+    {
+        ES3.Save("sk", _inventory);
+    }
+
+    [ContextMenu("Load")]
+    private void Load()
+    {
+        _inventory = ES3.Load("sk", new Inventory());
+    }
+}
