2d46d8f21fb7b89a31cce25a9fa2fe0b9360fef1
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2022-01-25 20:32:02 +0200

    Refactoring

---
+++ UnityInventory/Assets/Inventory/Runtime/Items/ItemSO.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace InventorySystem
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+    public class ItemSO : ScriptableObject
+    {
+        [SerializeField] private byte _id;
+        [Tooltip("Will be used to display the name in the UI.")] 
+        [SerializeField] private string _itemName;
+        [SerializeField] private string _glyph;
+        [SerializeField] private Sprite _normalIcon;
+        [SerializeField] private Sprite _lockedIcon;
+
+        public byte ID => _id;
+        public string ItemName => _itemName;
+        public string Glyph => _glyph;
+
+        public virtual Sprite GetIcon(IconType type = IconType.Normal)
+        {
+            switch (type)
+            {
+                case IconType.Locked:
+                    return _lockedIcon;
+            }
+
+            return _normalIcon;
+        }
+    }
+}
