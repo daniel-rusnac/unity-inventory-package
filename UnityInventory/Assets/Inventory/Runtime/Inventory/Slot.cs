225d9206ad3ead7157851dc374c6cac32046e2d1
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2022-02-12 00:18:16 +0200

    Simplified inventory slots

--- UnityInventory/Assets/Inventory/Runtime/Inventory/Slot.cs
+++ UnityInventory/Assets/Inventory/Runtime/Inventory/Slot.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace InventorySystem
 {
     [Serializable]
-    public struct Slot : IEquatable<Slot>
+    public struct Slot
     {
         [SerializeField] private int _count;
         [SerializeField] private int _max;
@@ -28,58 +28,9 @@ namespace InventorySystem
             return new Slot(_id, _count, value);
         }
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) 
-                return false;
-            
-            return obj is Slot && Equals((Slot) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hashCode = _id.GetHashCode();
-                hashCode = (hashCode * 397) ^ _count;
-                hashCode = (hashCode * 397) ^ _max;
-                return hashCode;
-            }
-        }
-
-        public static bool operator ==(Slot a, Slot b)
-        {
-            return a.Equals(b);
-        }
-
-        public static bool operator !=(Slot a, Slot b)
-        {
-            return !(a == b);
-        }
-
-        public bool Equals(Slot other)
-        {
-            return _id == other._id && Count == other.Count;
-        }
-
         public static Slot operator +(Slot packet, int count)
         {
             return new Slot(packet._id, ClampCount(packet.Count + count, packet._max), packet._max);
-        }
-
-        public static Slot operator *(Slot packet, int multiplier)
-        {
-            return new Slot(packet._id, ClampCount(packet.Count * multiplier, packet._max), packet._max);
-        }
-
-        public static Slot operator /(Slot packet, int divisor)
-        {
-            if (divisor == 0)
-            {
-                return new Slot(packet._id, packet.Count, packet._max);
-            }
-
-            return new Slot(packet._id, ClampCount(packet.Count / divisor, packet._max), packet._max);
         }
 
         public static Slot operator -(Slot packet, int count)
@@ -102,7 +53,7 @@ namespace InventorySystem
         public override string ToString()
         {
             InventoryUtility.TryGetItem(_id, out ItemSO item);
-            return $"{(item == null ? "unknown" : item.ItemName)}: {Count}{(_max > 0 ? $"/{_max}" : "")}";
+            return $"{(item == null ? "???" : item.ItemName)}: {Count}{(_max > 0 ? $"/{_max}" : "")}";
         }
     }
 }
