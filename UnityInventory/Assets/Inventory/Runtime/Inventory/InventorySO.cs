e33edcf9e75c6f1f1d44374e959e769f88e9f5d5
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2022-02-12 00:17:59 +0200

    Inventory now works only with items directly

--- UnityInventory/Assets/Inventory/Runtime/Inventory/InventorySO.cs
+++ UnityInventory/Assets/Inventory/Runtime/Inventory/InventorySO.cs
@@ -8,174 +8,118 @@ namespace InventorySystem
     [CreateAssetMenu(fileName = "Inventory", menuName = "Inventory/Inventory")]
     public class InventorySO : ScriptableObject
     {
-        private HashSet<Action> _onChangedSubscribers = new HashSet<Action>();
-        private Dictionary<int, Slot> _slotByID = new Dictionary<int, Slot>();
-        
-        [Obsolete("Use the inventory directly.")]
-        public InventorySO GetInventory => this;
+        public event Action Changed;
+        public event Action<ItemSO, int> ChangedAmount;
 
+        private readonly Dictionary<ItemSO, Slot> _slotByID = new Dictionary<ItemSO, Slot>();
+        
         public void Add(ItemSO item, int amount)
         {
-            Add(item.ID, amount);
-        }
-
-        public void Add(int id, int amount)
-        {
-            if (amount <= 0)
+            if (amount == 0)
                 return;
+            
+            if (amount < 0)
+                Remove(item, amount);
 
-            if (!_slotByID.ContainsKey(id))
+            if (!_slotByID.ContainsKey(item))
             {
-                _slotByID.Add(id, new Slot(id, 0));
+                _slotByID.Add(item, new Slot(item.ID, 0));
             }
 
-            _slotByID[id] += amount;
-            OnChanged();
+            _slotByID[item] += amount;
         }
-
+        
         public void Remove(ItemSO item, int amount)
         {
-            Remove(item.ID, amount);
-        }
-
-        public void Remove(int id, int amount)
-        {
-            if (amount <= 0 || !_slotByID.ContainsKey(id))
+            if (amount == 0)
                 return;
 
-            _slotByID[id] -= amount;
-            OnChanged();
+            if (amount < 0)
+            {
+                Add(item, -amount);
+                return;
+            }
+            
+            if (!_slotByID.ContainsKey(item))
+                return;
+
+            _slotByID[item] -= amount;
         }
 
-        /// <summary>
-        /// Check if the inventory contains an item.
-        /// </summary>
-        /// <param name="item">Item to check.</param>
-        /// <param name="amount">The expected amount.</param>
-        /// <returns>True if the item count is >= to the expected amount. 1 by default.</returns>
         public bool Contains(ItemSO item, int amount = 1)
-        {
-            return Contains(item.ID, amount);
-        }
-
-        /// <summary>
-        /// Check if the inventory contains an item.
-        /// </summary>
-        /// <param name="id">Item ID to check.</param>
-        /// <param name="amount">The expected amount.</param>
-        /// <returns>True if the item count is >= to the expected amount. 1 by default.</returns>
-        public bool Contains(int id, int amount = 1)
         {
             if (amount <= 0)
                 return true;
 
-            return _slotByID.ContainsKey(id) && _slotByID[id].Count >= amount;
+            return _slotByID.ContainsKey(item) && _slotByID[item].Count >= amount;
         }
 
-        public int GetCount(ItemSO itemSO)
+        public int GetCount(ItemSO item)
         {
-            return GetCount(itemSO.ID);
-        }
-
-        public int GetCount(int id)
-        {
-            if (_slotByID.ContainsKey(id))
+            if (_slotByID.ContainsKey(item))
             {
-                return _slotByID[id].Count;
+                return _slotByID[item].Count;
             }
 
             return 0;
         }
 
-        public int GetMax(ItemSO itemSO)
+        /// <summary>
+        /// Set the maximum value for an item.
+        /// </summary>
+        /// <param name="item">Item ID to set maximum valur for.</param>
+        /// <param name="max">The maximum value. -1 is unlimited.</param>
+        public void SetMax(ItemSO item, int max)
         {
-            return GetMax(itemSO.ID);
+            if (!_slotByID.ContainsKey(item))
+            {
+                _slotByID.Add(item, new Slot(item.ID, 0, max));
+            }
+            else
+            {
+                _slotByID[item] = _slotByID[item].SetMax(max);
+            }
         }
 
-        public int GetMax(int id)
+        public int GetMax(ItemSO item)
         {
-            if (_slotByID.ContainsKey(id))
+            if (_slotByID.ContainsKey(item))
             {
-                return _slotByID[id].Count;
+                return _slotByID[item].Count;
             }
 
             return InventoryUtility.DEFAULT_MAX;
         }
 
-        /// <summary>
-        /// Set the maximum value for an item.
-        /// </summary>
-        /// <param name="itemSO">Item to set maximum valur for.</param>
-        /// <param name="max">The maximum value. -1 is unlimited.</param>
-        public void SetMax(ItemSO itemSO, int max)
-        {
-            SetMax(itemSO.ID, max);
-        }
-
-        /// <summary>
-        /// Set the maximum value for an item.
-        /// </summary>
-        /// <param name="id">Item ID to set maximum valur for.</param>
-        /// <param name="max">The maximum value. -1 is unlimited.</param>
-        public void SetMax(int id, int max)
-        {
-            if (!_slotByID.ContainsKey(id))
-            {
-                _slotByID.Add(id, new Slot(id, 0, max));
-            }
-            else
-            {
-                _slotByID[id] = _slotByID[id].SetMax(max);
-            }
-
-            OnChanged();
-        }
-
-        public int[] GetAllIDs()
-        {
-            return _slotByID.Where(pair => pair.Value.Count > 0).Select(pair => pair.Key).ToArray();
-        }
-
-        public ItemSO[] GetAllItems()
-        {
-            return _slotByID.Where(pair => pair.Value.Count > 0).Select(pair =>
-            {
-                InventoryUtility.TryGetItem(pair.Key, out ItemSO item);
-                return item;
-            }).ToArray();
-        }
-
-        public int GetSlotCount()
+        /// <returns>How many slots with count > 0 are in the inventory.</returns>
+        public int GetActiveSlotsCount()
         {
             return _slotByID.Count(pair => pair.Value.Count > 0);
         }
 
+        public ItemSO[] GetItems()
+        {
+            return _slotByID.Where(pair => pair.Value.Count > 0).Select(pair => pair.Key).ToArray();
+        }
+
+        /// <summary>
+        /// Removes all items from the inventory.
+        /// </summary>
         public void Clear()
         {
+            foreach (ItemSO item in _slotByID.Keys)
+            {
+                OnChanged(item, _slotByID[item].Count);
+            }
+            
             _slotByID.Clear();
             OnChanged();
         }
-
-        /// <summary>
-        /// Subscribe to the OnChanged event. Called every time the inventory is modified.
-        /// </summary>
-        public void Register(Action action)
-        {
-            _onChangedSubscribers.Add(action);
-        }
-
-        /// <summary>
-        /// Unsubscribe from the OnChanged event. Called every time the inventory is modified.
-        /// </summary>
-        public void Unregister(Action action)
-        {
-            _onChangedSubscribers.Remove(action);
-        }
-
+        
         public override string ToString()
         {
             if (_slotByID.Count == 0)
-                return "Empty";
+                return "[Empty]";
 
             string result = "[";
 
@@ -183,13 +127,37 @@ namespace InventorySystem
 
             return result + "]";
         }
+        
+        private void OnChanged(ItemSO item, int delta)
+        {
+            ChangedAmount?.Invoke(item, delta);
+            OnChanged();
+        }
 
         private void OnChanged()
         {
-            foreach (Action subscriber in _onChangedSubscribers)
-            {
-                subscriber?.Invoke();
-            }
+            Changed?.Invoke();
+        }
+
+        [Obsolete("Use the inventory directly.")]
+        public InventorySO GetInventory => this;
+
+        [Obsolete("Use GetItems instead.")]
+        public ItemSO[] GetAllItems()
+        {
+            return GetItems();
+        }
+
+        [Obsolete("Use Inventory.Changed instead.")]
+        public void Register(Action action)
+        {
+            Changed += action;
+        }
+
+        [Obsolete("Use Inventory.Changed instead.")]
+        public void Unregister(Action action)
+        {
+            Changed -= action;
         }
     }
 }
--- UnityInventory/Assets/Inventory/Runtime/InventoryExtensions.cs
+++ UnityInventory/Assets/Inventory/Runtime/InventoryExtensions.cs
@@ -11,21 +11,9 @@
         /// <param name="amount">The amount to transfer. Will not add more items than removed.</param>
         public static void TransferTo(this InventorySO from, InventorySO to, ItemSO item, int amount)
         {
-            TransferTo(from, to, item.ID, amount);
-        }
-        
-        /// <summary>
-        /// Removed the items from an inventory and ads them to another.
-        /// </summary>
-        /// <param name="from">Inventory to remove items from.</param>
-        /// <param name="to">Inventory to add items to.</param>
-        /// <param name="itemID">The ID of the item to transfer.</param>
-        /// <param name="amount">The amount to transfer. Will not add more items than removed.</param>
-        public static void TransferTo(this InventorySO from, InventorySO to, int itemID, int amount)
-        {
-            int itemsRemoved = from.GetCount(itemID);
-            from.Remove(itemID, amount);
-            to.Add(itemID, itemsRemoved);
+            int itemsRemoved = from.GetCount(item);
+            from.Remove(item, amount);
+            to.Add(item, itemsRemoved);
         }
     }
 }
