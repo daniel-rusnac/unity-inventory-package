c03a60c12718c64ae662038dd38109999194ecd1
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2022-02-14 01:53:28 +0200

    Added item extensions

---
+++ UnityInventory/Assets/Inventory/Runtime/Extensions/ItemExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace InventorySystem
+{
+    public static class ItemExtensions
+    {
+        /// <summary>
+        /// Check is the inventory has at least on item.
+        /// </summary>
+        public static bool IsUnlocked(this ItemSO item, InventorySO inventory)
+        {
+            return inventory.Contains(item);
+        }
+    }
+}
