b0ed380bb21110c4a73f16551abac0fc4d5a46a2
Daniel <daniel.rusnac.personal@gmail.com>
2022-02-24 10:32:26 +0200

    Update slots

---
+++ UnityInventory/Assets/Inventory/Runtime/Inventories/Slot.cs
@@ -0,0 +1,19 @@
+﻿namespace InventorySystem.Slots
+{
+    public class Slot
+    {
+        public bool ShouldBeRemoved => Amount <= 0;
+        private int _staticID;
+        private int _dynamicID;
+
+        public int StaticID => _staticID;
+        public int DynamicID => _dynamicID;
+        public long Amount { get; set; }
+
+        public Slot(int staticID, int dynamicID)
+        {
+            _staticID = staticID;
+            _dynamicID = dynamicID;
+        }
+    }
+}
