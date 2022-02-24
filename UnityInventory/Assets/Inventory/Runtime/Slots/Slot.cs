a8e86d1c81f7e351128520f65fac897f8cb0e8d5
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2022-02-24 22:21:50 +0200

    Revert "Revert "Update limits""
    This reverts commit 7daa599dd2caa9dd2a50306f9bc0d32fbdae6b02.

--- UnityInventory/Assets/Inventory/Runtime/Slots/Slot.cs
+++ UnityInventory/Assets/Inventory/Runtime/Slots/Slot.cs
@@ -5,11 +5,6 @@
         public bool ShouldBeRemoved => Amount <= 0;
         public abstract int StaticID { get; }
         public abstract int DynamicID { get; }
-        public abstract long Amount { get; }
-        public abstract long Limit { get; }
-        
-        public abstract void Add(long amount);
-        public abstract void Remove(long amount);
-        public abstract void SetLimit(long limit);
+        public abstract long Amount { get; set; }
     }
 }
